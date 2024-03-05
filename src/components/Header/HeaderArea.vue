654e9b3c9e105280c8c9cb7abea3cacde96797d6
lsbrel <lucas.81.gabriel@gmail.com>
2024-03-05 15:45:18 -0300

    ✨ feat: header componentizado

--- src/components/Header/HeaderArea.vue
+++ src/components/Header/HeaderArea.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { useSidebarStore } from '@/stores/sidebar'
 import DarkModeSwitcher from './DarkModeSwitcher.vue'
 import DropdownMessage from './DropdownMessage.vue'
 import DropdownNotification from './DropdownNotification.vue'
 import DropdownUser from './DropdownUser.vue'
-
-const { isSidebarOpen, toggleSidebar } = useSidebarStore()
+import HeaderHamburgerButton from './HeaderHamburgerButton.vue'
+import HeaderLogoSmartphone from './HeaderLogoSmartphone.vue'
 </script>
 
 <template>
@@ -14,47 +13,13 @@ const { isSidebarOpen, toggleSidebar } = useSidebarStore()
   >
     <div class="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
       <div class="flex items-center gap-2 sm:gap-4 lg:hidden">
-        <!-- Hamburger Toggle BTN -->
-        <button
-          class="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
-          @click="
-            () => {
-              console.log('Toggling Sidebar')
-              toggleSidebar()
-            }
-          "
-        >
-          <span class="relative block h-5.5 w-5.5 cursor-pointer">
-            <span class="du-block absolute right-0 h-full w-full">
-              <span
-                class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white"
-                :class="{ '!w-full delay-300': !isSidebarOpen }"
-              ></span>
-              <span
-                class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white"
-                :class="{ '!w-full delay-400': !isSidebarOpen }"
-              ></span>
-              <span
-                class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white"
-                :class="{ '!w-full delay-500': !isSidebarOpen }"
-              ></span>
-            </span>
-            <span class="du-block absolute right-0 h-full w-full rotate-45">
-              <span
-                class="absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white"
-                :class="{ '!h-0 delay-[0]': !isSidebarOpen }"
-              ></span>
-              <span
-                class="delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white"
-                :class="{ '!h-0 dealy-200': !isSidebarOpen }"
-              ></span>
-            </span>
-          </span>
-        </button>
-        <!-- Hamburger Toggle BTN -->
-        <router-link class="block flex-shrink-0 lg:hidden" to="/">
-          <img src="@/assets/images/logo/logo-icon.svg" alt="Logo" />
-        </router-link>
+        <!-- BOTAO DE QUANDO TELA ESTA REDUZIDA -->
+        <HeaderHamburgerButton />
+        <!-- BOTAO DE QUANDO TELA ESTA REDUZIDA -->
+
+        <!-- LOGO COM TELA REDUZIDA -->
+        <HeaderLogoSmartphone />
+        <!-- LOGO COM TELA REDUZIDA -->
       </div>
 
       <div class="flex flex-row w-full justify-end gap-3 2xsm:gap-7">
---
+++ src/components/Header/HeaderLogoSmartphone.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <router-link class="block flex-shrink-0 lg:hidden" to="/">
+    <img src="@/assets/images/logo/logo-icon.svg" alt="Logo" />
+  </router-link>
+</template>
