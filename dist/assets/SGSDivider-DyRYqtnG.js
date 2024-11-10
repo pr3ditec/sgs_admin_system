057e8a158eca3886da4cf9085cb321a77d11cbb7
lsbrel <lucas.81.gabriel@gmail.com>
2024-11-10 17:23:26 -0300

    ✨ feat: new build

--- dist/assets/SGSDivider-DyRYqtnG.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./Response-TKM2zK-M.js";import{f as e,c as o}from"./index-sOqKN4X3.js";const r={},s={class:"p-2"};function t(n,_){return e(),o("div",s)}const f=c(r,[["render",t]]);export{f as S};
