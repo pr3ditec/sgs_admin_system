f316960eb12c60291183655802268056bf1df079
lsbrel <lucas.81.gabriel@gmail.com>
2024-11-18 14:36:51 -0300

    build

---
+++ dist/assets/SGSDivider-Cdrz1sw5.js
@@ -0,0 +1,1 @@
+import{a as c}from"./Response-Cdpp2_KF.js";import{f as e,c as o}from"./index-CtdWVGE4.js";const r={},s={class:"p-2"};function t(n,_){return e(),o("div",s)}const f=c(r,[["render",t]]);export{f as S};
